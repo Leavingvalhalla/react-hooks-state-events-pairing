48d7a3be7140f74d7d9f7fe5cb9a5acca045c3f0
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-21 14:38:30 -0700

    add search function

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Comment from './Comment';
 
 function Comments({ data }) {
+  const [query, setQuery] = useState('');
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center', paddingTop: '2%' }}>
+      <input
+        onChange={(e) => handleChange(e)}
+        type="text"
+        placeholder="Search by username"
+        value={query}
+      />
       <h3>{`${data.comments.length} comments`}</h3>
-      {data.comments.map((commentInfo) => {
-        return <Comment commentInfo={commentInfo} />;
-      })}
+      {data.comments
+        .filter((commentInfo) => {
+          console.log(commentInfo.user);
+          console.log(query);
+          return commentInfo.user.includes(query) || query === '';
+        })
+        .map((commentInfo) => {
+          return <Comment commentInfo={commentInfo} query={query} />;
+        })}
     </div>
   );
 }
